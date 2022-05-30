2a89a2c0c1b2cde2e36081b77588dd8890ba4fb6
Nitesh Agarwala <niteshagarwal003@gmail.com>
2022-05-30 09:21:49 +0530

    Create, new action, validation and flash messsages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,24 @@ class ArticlesController < ApplicationController
        # byebug
         @article = Article.find(params[:id])
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        # render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        # render plain: @article.inspect
+       if @article.save
+         flash[:notice] = "Article was saved successfully"
+         redirect_to @article
+       else
+        render 'new'
+       end
+    end
 end
