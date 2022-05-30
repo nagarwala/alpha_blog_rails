e8a0815fd08af786f81f7519376e16952648126c
Nitesh Agarwala <niteshagarwal003@gmail.com>
2022-05-30 16:22:16 +0530

    Create user table and user model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show,:edit,:update,:destroy]
+
     def show
        # byebug
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -12,9 +14,12 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+    end
+
     def create
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title,:description))
+        @article = Article.new(article_params)
         # render plain: @article.inspect
        if @article.save
          flash[:notice] = "Article was saved successfully"
@@ -23,4 +28,28 @@ class ArticlesController < ApplicationController
         render 'new'
        end
     end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+    def set_article
+        @article = Article.find(params[:id]) 
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+
 end
